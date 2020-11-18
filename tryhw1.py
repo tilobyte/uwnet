e70baa80eadaa2c340d2d9eb6a6192c724ebaf19
Timothy Akintilo <timtobi1@gmail.com>
2020-11-18 02:29:22 -0800

    ipynb

--- tryhw1.py
+++ tryhw1.py
@@ -18,6 +18,7 @@ def conv_net():
         make_connected_layer(256, 10),
         make_activation_layer(SOFTMAX),
     ]
+    print("using conv_net")
     return make_net(l)
 
 
@@ -37,6 +38,27 @@ def conv_net():
 #     return make_net(l)
 
 
+def conn_net():
+    l = [
+        make_connected_layer(32 * 32 * 3, 256),
+        make_activation_layer(RELU),
+        make_connected_layer(256, 128),
+        make_activation_layer(RELU),
+        make_connected_layer(128, 256),
+        make_activation_layer(RELU),
+        make_connected_layer(256, 450),
+        make_activation_layer(RELU),
+        make_connected_layer(450, 256),
+        make_activation_layer(RELU),
+        make_connected_layer(256, 128),
+        make_activation_layer(RELU),
+        make_connected_layer(128, 10),
+        make_activation_layer(SOFTMAX),
+    ]
+    print("using conn_net")
+    return make_net(l)
+
+
 print("loading data...")
 train = load_image_classification_data("cifar/cifar.train", "cifar/cifar.labels")
 test = load_image_classification_data("cifar/cifar.test", "cifar/cifar.labels")
@@ -50,7 +72,7 @@ rate = 0.01
 momentum = 0.9
 decay = 0.005
 
-m = conv_net()
+m = conn_net()
 print("training...")
 train_image_classifier(m, train, batch, iters, rate, momentum, decay)
 print("done")
@@ -64,3 +86,24 @@ print("test accuracy:     %f", accuracy_net(m, test))
 # Why are you seeing these results? Speculate based on the information you've gathered and what you know about DL and ML.
 # Your answer:
 #
+# The convnet makes 8 * 28 * 1024 + 16 * 72 * 16^2 + 32 * 144 * 64
+# + 64 * 288 * 16 + 10 * 256 * 1 = 1108480 operations.
+#
+# The fully connected network makes 3072*256 + 256*128 + 128*256
+# + 256*450 + 450*256 + 256*128 + 128*10 = 1116416 operations.
+#
+# convnet training accuracy:         %f 0.6713799834251404
+# convnet test accuracy:             %f 0.6280999779701233
+# fully connected training accuracy: %f 0.5281400084495544
+# fully connected test accuracy:     %f 0.48809999227523804
+#
+# I suspect the convnet performs better due to its ability to process spatial information about each image.
+# In the fully connected network, every neuron in the first layer is connected to every neuron in the
+# second layer, every neuron in the second layer is connected to every neuron in the third layer, etc. This
+# causes spatial information contained within the image to be lost, as each pixel is considered equally related
+# to every other pixel, regardless of the distance between them. On the other hand, the use of convolutions
+# in the convnet causes the connections in the network to be sparser. Pixels that are close to each other
+# are considered more related to one another than pixels far away from each other, since we are using
+# weighted sums of small areas in each image.
+# Since spatial information is so important to understanding what an image depicts, the fact that the convnet
+# can use this information causes it to outperform the fully connected network.
